22f71f75d67da7df10c7ae05b64103834a862f5d
Shripad Joshi (C) <sjoshi@splunk.com>
2018-10-13 08:03:10 +0530

    optimized the code for graphs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,36 @@
 class HomeController < ApplicationController
   def index
-    total_sell_today = Sim.where(sell_date: Date.today)
-    @prepaid_daily_sale_cnt = total_sell_today.where(sim_type: 'Prepaid').count
-    @postpaid_daily_sale_cnt = total_sell_today.where(sim_type: 'Postpaid').count
+    start_month_date = Date.today.beginning_of_month
+    end_month_date = Date.today.end_of_month
+    monthly_sale = Sim.where(sell_date: start_month_date..end_month_date)
+                             .order(:sell_date)
+                             .group_by{|data| [data.sell_date.to_date.strftime('%d %b, %Y'),data.sim_type]}
+    
+    today_date = Date.today.strftime('%d %b, %Y')
+    @prepaid_daily_sale_cnt = (monthly_sale[["#{today_date}", "Prepaid"]] ? monthly_sale[["#{today_date}", "Prepaid"]].count : 0)
+    @postpaid_daily_sale_cnt = (monthly_sale[["#{today_date}", "Postpaid"]] ? monthly_sale[["#{today_date}", "Postpaid"]].count : 0)
+
     
     start_date = Date.today.beginning_of_week
     end_date = Date.today.end_of_week
     @prepaid_sale = []
     @postpaid_sale = []
     @dates = []
+    
     for i in start_date..end_date
+      @prepaid_sale << (monthly_sale[["#{i.strftime('%d %b, %Y')}", "Prepaid"]] ? monthly_sale[["#{i.strftime('%d %b, %Y')}", "Prepaid"]].count : 0)
+      @postpaid_sale << (monthly_sale[["#{i.strftime('%d %b, %Y')}", "Postpaid"]] ? monthly_sale[["#{i.strftime('%d %b, %Y')}", "Postpaid"]].count : 0)
       @dates.push("#{i.strftime('%d %b, %Y')}")
-      @prepaid_sale.push((Sim.where(sell_date: i).where(sim_type: 'Prepaid').count).to_i)
-      @postpaid_sale.push((Sim.where(sell_date: i).where(sim_type: 'Postpaid').count).to_i)
     end
 
-    start_month_date = Date.today.beginning_of_month
-    end_month_date = Date.today.end_of_month
     @prepaid_month_sale = []
     @postpaid_month_sale = []
     @month_dates = []
+
     for i in start_month_date..end_month_date
+      @prepaid_month_sale << (monthly_sale[["#{i.strftime('%d %b, %Y')}", "Prepaid"]] ? monthly_sale[["#{i.strftime('%d %b, %Y')}", "Prepaid"]].count : 0)
+      @postpaid_month_sale << (monthly_sale[["#{i.strftime('%d %b, %Y')}", "Postpaid"]] ? monthly_sale[["#{i.strftime('%d %b, %Y')}", "Postpaid"]].count : 0)
       @month_dates.push("#{i.strftime('%d %b, %Y')}")
-      @prepaid_month_sale.push((Sim.where(sell_date: i).where(sim_type: 'Prepaid').count).to_i)
-      @postpaid_month_sale.push((Sim.where(sell_date: i).where(sim_type: 'Postpaid').count).to_i)
     end
 
     @quarter = ['Q1 (Apr-June)', 'Q2 (July-Sept)', 'Q3 (Oct-Dec)', 'Q4 (Jan-Mar)']
@@ -43,17 +50,22 @@ class HomeController < ApplicationController
     @prepaid_qty_sale = []
     @postpaid_qty_sale = []
 
-    @prepaid_qty_sale.push((Sim.where(sell_date: q2_start..q2_end).where(sim_type: 'Prepaid').count).to_i)
-    @postpaid_qty_sale.push((Sim.where(sell_date: q2_start..q2_end).where(sim_type: 'Postpaid').count).to_i)
+    q1_sale = Sim.where(sell_date: q1_start..q1_end).group(:sim_type).count
+    q2_sale = Sim.where(sell_date: q2_start..q2_end).group(:sim_type).count
+    q3_sale = Sim.where(sell_date: q3_start..q3_end).group(:sim_type).count
+    q4_sale = Sim.where(sell_date: q4_start..q4_end).group(:sim_type).count
 
-    @prepaid_qty_sale.push((Sim.where(sell_date: q3_start..q3_end).where(sim_type: 'Prepaid').count).to_i)
-    @postpaid_qty_sale.push((Sim.where(sell_date: q3_start..q3_end).where(sim_type: 'Postpaid').count).to_i)
+    @prepaid_qty_sale.push((q2_sale["Prepaid"] ? q2_sale["Prepaid"] : 0))
+    @postpaid_qty_sale.push((q2_sale["Postpaid"] ? q2_sale["Postpaid"] : 0))
 
-    @prepaid_qty_sale.push((Sim.where(sell_date: q4_start..q4_end).where(sim_type: 'Prepaid').count).to_i)
-    @postpaid_qty_sale.push((Sim.where(sell_date: q4_start..q4_end).where(sim_type: 'Postpaid').count).to_i)
+    @prepaid_qty_sale.push((q3_sale["Prepaid"] ? q3_sale["Prepaid"] : 0))
+    @postpaid_qty_sale.push((q3_sale["Postpaid"] ? q3_sale["Postpaid"] : 0))
 
-    @prepaid_qty_sale.push((Sim.where(sell_date: q1_start..q1_end).where(sim_type: 'Prepaid').count).to_i)
-    @postpaid_qty_sale.push((Sim.where(sell_date: q1_start..q1_end).where(sim_type: 'Postpaid').count).to_i)
+    @prepaid_qty_sale.push((q4_sale["Prepaid"] ? q4_sale["Prepaid"] : 0))
+    @postpaid_qty_sale.push((q4_sale["Postpaid"] ? q4_sale["Postpaid"] : 0))
+
+    @prepaid_qty_sale.push((q1_sale["Prepaid"] ? q1_sale["Prepaid"] : 0))
+    @postpaid_qty_sale.push((q1_sale["Postpaid"] ? q1_sale["Postpaid"] : 0))
 
   end
 end
